82e9f3582f5641e809a422637699ce1693eb075e
jjhernandez <jjhernandez@CIGLMP127KMA>
2018-08-30 14:52:38 +0800

    Add project files.

---
+++ LottieAnimation/LottieAnimation/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LottieAnimation
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void NextOnclicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ViewA());
+        }
+    }
+}
